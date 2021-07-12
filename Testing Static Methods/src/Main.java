5064ff6be79435ee75fa1ee8c12244b1677a9b42
NguyenTimothy <87158137+NguyenTimothy@users.noreply.github.com>
2021-07-11 22:58:43 -0700

    Testing Static Methods

---
+++ Testing Static Methods/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    static double absValue(double num){
+        if(num < 0){
+            num -= num + num;
+        }
+        return num;
+    }
+    static double squareRoot(double num){
+        double low = 0;
+        double high = num;
+        double mid = (high + low)/2;
+        while(absValue(mid*mid - num) > 0.0001){
+            mid = (high + low)/2;
+            if(mid*mid > num){
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+        return mid;
+    }
+    static double pyth(double a, double b){
+        return squareRoot(a*a + b*b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(absValue(-5));
+        System.out.println(squareRoot(25));
+        System.out.println(pyth(4, 3));
+    }
+}
